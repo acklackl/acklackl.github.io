5b47054db041d42e82a05d2f590607d9ea6eb8c3
Fake <acleung@cpp.edu>
2017-03-08 23:46:20 -0800

    Improved response

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ window.sr = ScrollReveal();
 sr.reveal(".MainTitle", { duration: 1000,
     easing: "cubic-bezier(0.17, 0.67, 0.87, 0.39)",
     origin: "top",
+    mobile: false
 });
 
 sr.reveal("nav span", { duration: 500,
     delay: 1000,
     origin: "top",
+    mobile: false
 }, 80);
 
 
@@ -15,61 +17,61 @@ sr.reveal(".bio-title", {
     origin: "left",
     distance: "100px",
     duration: 1500,
-    reset: true,
     rotate: {z: 30},
+    reset: true
 });
 
 sr.reveal(".bio-content", {
     origin: "left",
     duration: 1000,
-    reset: true,
+    mobile: false
 });
 
 sr.reveal(".git-title", {
     origin: "right",
     duration: 2000,
-    reset: true,
-    rotate: {z: -50}
+    rotate: {z: -50},
+    reset: true
 });
 
 sr.reveal(".music-title", {
     origin: "left",
     duration: 2000,
-    reset: true,
-    rotate: {z: 50}
+    rotate: {z: 50},
+    reset: true
 });
 
 sr.reveal(".git-logo", {
-    reset: true,
-    duration: 1500
+    duration: 1500,
+    mobile: false
 });
 
 sr.reveal(".music-logo", {
-    reset: true,
-    duration: 1500
+    duration: 1500,
+    mobile: false
 });
 
 sr.reveal(".git-section p", {
     origin: "left",
     duration: 1000,
-    reset: true
+    mobile: false
 }, 150);
 
 sr.reveal(".git-section-title", {
     origin: "right",
     distance: "1000px",
     duration: 1000,
-    reset: true
+    mobile: false
 }, 150);
 
 sr.reveal(".music-content-title", {
     origin: "left",
     distance: "1000px",
     duration: 1000,
-    reset: true
+    mobile: false
 });
 
 sr.reveal("iframe", {
-    reset: true,
-    duration: 1500
+    duration: 1500,
+    mobile: false
 }, 150);

acbc9789bcf726431c25f1b02430652ce42ef210
Fake <acleung@cpp.edu>
2017-03-09 19:45:45 -0800

    Some issues with disappearing music logo and mainTitle, still working on it.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,49 @@
 window.sr = ScrollReveal();
 
+//width of current screen
+let width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
-var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-
-function remove(id) {
-    var elem = document.getElementById(id);
+//function to remove an element
+let remove = (id) => {
+    let elem = document.getElementById(id);
     return elem.parentNode.removeChild(elem);
-}
+};
+
+//function that reveals music logo, main title, and nav if width is greater than 700. else, music logo is removed
+//and visibility for MainTitle and nav span are set to hidden
+let reveal = () => {
+    if (width > 700) {
+        sr.reveal(".music-logo", {
+            duration: 1500,
+            mobile: false
+        });
+
+        sr.reveal(".MainTitle", {
+            duration: 1000,
+            easing: "cubic-bezier(0.17, 0.67, 0.87, 0.39)",
+            origin: "top",
+            mobile: false
+        });
+
+        sr.reveal("nav span", {
+            duration: 500,
+            delay: 1000,
+            origin: "top",
+            mobile: false
+        }, 80);
+    }
+
+    else {
+        remove("music-logo");
+        //doesn't work
+        //document.getElementsByClassName("MainTitle").style.visibility = "hidden";
+    }
+};
+
+window.addEventListener('resize', event => {
+    width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    reveal();
+});
 
 sr.reveal(".bio-title", {
     origin: "left",
@@ -41,28 +78,7 @@ sr.reveal(".git-logo", {
     mobile: false
 });
 
-if (width > 700) {
-    sr.reveal(".music-logo", {
-        duration: 1500,
-        mobile: false
-    });
-
-    sr.reveal(".MainTitle", { duration: 1000,
-        easing: "cubic-bezier(0.17, 0.67, 0.87, 0.39)",
-        origin: "top",
-        mobile: false
-    });
-
-    sr.reveal("nav span", { duration: 500,
-        delay: 1000,
-        origin: "top",
-        mobile: false
-    }, 80);
-}
-
-else {
-    remove("music-logo");
-}
+reveal();
 
 sr.reveal(".git-section p", {
     origin: "left",
